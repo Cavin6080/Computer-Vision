85d40c44663831fb755d2f802ad9ff989a6802af
Cavin <50316190+Cavin6080@users.noreply.github.com>
2021-04-11 17:04:55 +0530

    Created object tracking programme
    It uses CSRT algorithm to track objects

---
+++ Object_tracking/object_tracking.py
@@ -0,0 +1,34 @@
+# Importing the libraries
+import cv2
+
+# Creating tracker
+tracker = cv2.TrackerCSRT_create()
+
+# Reading video
+video = cv2.VideoCapture('race.mp4')
+##Read first frame of the video
+success, frame = video.read()
+
+# Track region on first frame and for drawing bounding box on that frame
+bbox = cv2.selectROI(frame)
+
+# Track the first frame
+success = tracker.init(frame, bbox)
+
+# Reading and tracking all frames
+while True:
+    success, frame = video.read()
+    if not success:
+        break
+    sucess, bbox = tracker.update(frame)
+
+    # draw bounding box
+    if sucess:
+        (x, y, w, h) = [int(i) for i in bbox]  # Copying Information of bbox
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2, 1)
+    else:
+        cv2.putText(frame, 'Error', (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+    cv2.imshow('Tracking', frame)
+    if cv2.waitKey(1) & 0XFF == 27:  # ESC key
+        break
